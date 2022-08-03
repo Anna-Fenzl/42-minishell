8452ca5c4c0e7bd1f0446b3359a3773f092aad69
Anna-selina Fenzl <afenzl@1-C-30.42heilbronn.de>
2022-08-03 18:03:38 +0200

    builtin pwd done

--- executor/execute.h
+++ executor/execute.h
@@ -6,7 +6,7 @@
 /*   By: afenzl <afenzl@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 17:06:08 by afenzl            #+#    #+#             */
-/*   Updated: 2022/08/03 17:55:12 by afenzl           ###   ########.fr       */
+/*   Updated: 2022/08/03 18:03:20 by afenzl           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define EXECUTE_H
 
 # include <unistd.h>
-#include <stdio.h>
+# include <stdio.h>
 # include <stdlib.h>
 # include <errno.h>
 # include "../libs/libs.h"
